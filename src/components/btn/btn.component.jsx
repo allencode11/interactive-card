b75858fd9a3bab3daa083555d03c0d4a8a097212
Alina Pavalachi <al.el.en.lina@gmail.com>
2023-12-11 01:02:32 +0200

    adding func to btn

--- src/components/btn/btn.component.jsx
+++ src/components/btn/btn.component.jsx
@@ -2,6 +2,6 @@ import './btn.component.css';
 
 export const BtnComponent = ({text = 'Confirm', func}) => {
   return (
-    <div className='btn'>{text}</div>
+    <div className='btn' onClick={func}>{text}</div>
   );
 };
